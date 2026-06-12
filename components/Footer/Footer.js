14ad7d8a4c8bd3c09597a3b36cd258204dab3dd8
valcio vasconceslos junior <158508027+Valcio01@users.noreply.github.com>
2026-06-12 20:02:10 -0300

    Update footer to project style

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,10 +4,9 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <p>&copy; {new Date().getFullYear()} TechCatalog. Todos os direitos reservados.</p>
+        <p>Catálogo Interativo de Produtos - Projeto de Programação Web I</p>
         <div className={styles.links}>
-          <a href="#">Privacidade</a>
-          <a href="#">Termos</a>
+          <a href="/contato">Fale Conosco</a>
         </div>
       </div>
     </footer>
